f83af7accd0f9be3aef5757f0d4cddd5d71bf167
YOLO\YoLo <f98989000@gmail.com>
2018-03-16 10:35:21 +0800

    Test ver1.1

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,8 +2,19 @@
 #include <string>
 #include <iostream>
 using namespace System;
-bool Guess(int number) {
-}
-int main(array<System::String ^> ^args)
+bool Guess(int number)
 {
+    return false;
+}
+int main(array<System::String^>^ args)
+{
+    int guess;
+
+    do
+    {
+        std::cin >> guess;
+    }
+    while (!Guess(guess));
+
+    return 0;
 }
